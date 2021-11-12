f17ff8b1b06cd6d8dde6c2c3f7119417f717131d
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-12 14:58:06 -0500

    pseudo code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
+// import inquirer
 const inquirer = require("inquirer");
+
+// questions list
+const questions = [{}];
+
+const generateTitle = (answers) => {
+  return ``;
+};
+
+const generateTableOfContents = (answers) => {
+  return ``;
+};
+const generateDescription = (answers) => {
+  return ``;
+};
+const generateInstallation = (answers) => {
+  return ``;
+};
+const generateUsage = (answers) => {
+  return ``;
+};
+const generateContributing = (answers) => {
+  return ``;
+};
+const generateLicense = (answers) => {
+  return ``;
+};
+
+const generateReadme = (answers) => {
+  //   call functions within placeholders, passing in answers data
+  return ``;
+};
+
+const init = async () => {
+  // prompt questions w/ inquirer
+
+  // generate README using answers
+  generateReadme(answers);
+
+  // write generated readme to a file
+};
+
+// run prompt questions
+init();
